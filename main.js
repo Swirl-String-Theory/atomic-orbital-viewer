dc31d7d1304bb3264e1cd9549e5befc482992474
a.koohjani <a.koohjani@gmail.com>
2024-10-12 19:32:34 +0330

    Adding panel for x/y axis

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import { AtomicOrbital } from "./src/atomic-orbital-viewer/atomic-orbital-viewer";
 
+main();
+
 function main() {
     render();
-        dragElement(document.getElementsByClassName("box")[0]);
-    accordionEllment(document.getElementsByClassName("box-header")[0])
+    drag(document.getElementsByClassName("box")[0]);
+    collapsible(document.getElementsByClassName("box-header")[0]);
+    drawAxis(document.getElementsByClassName("box-content")[0]);
 }
 
 function render() {
@@ -12,21 +15,21 @@ function render() {
     atomicOrbital.start();
 }
 
-function dragElement(elem) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, moveElem= document.getElementsByClassName(elem.className + "-move")[0];
-    if (moveElem) moveElem.onmousedown = dragMouseDown;
-    else elem.onmousedown = dragMouseDown;
+function drag(elem) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, moveElem = document.getElementsByClassName(elem.className + "-move")[0];
+    if (moveElem) moveElem.onmousedown = drag_md;
+    else elem.onmousedown = drag_md;
 
-    function dragMouseDown(e) {
+    function drag_md(e) {
         e = e || window.event;
         e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
+        document.onmouseup = drag_mu;
+        document.onmousemove = drag_mm;
     }
 
-    function elementDrag(e) {
+    function drag_mm(e) {
         e = e || window.event;
         e.preventDefault();
         pos1 = pos3 - e.clientX;
@@ -37,14 +40,14 @@ function dragElement(elem) {
         elem.style.left = (elem.offsetLeft - pos1) + "px";
     }
 
-    function closeDragElement() {
+    function drag_mu() {
         document.onmouseup = null;
         document.onmousemove = null;
     }
 }
 
-function accordionEllment(elem) {
-    var panel = elem.nextElementSibling;
+function collapsible(elem) {
+    let panel = elem.nextElementSibling;
     elem.firstElementChild.addEventListener("click", function () {
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
@@ -53,4 +56,24 @@ function accordionEllment(elem) {
         }
     });
 }
-main();
+
+function drawAxis(elem) {
+    let svg = elem.firstElementChild, height = svg.clientHeight - 10, width = svg.clientWidth - 10;
+    createLine(svg, width / 2, '10', width / 2, height, 'green', 'y');
+    createLine(svg, width / 2, '10', width / 2 + 5, '15', 'green', 'y1');
+    createLine(svg, width / 2, '10', width / 2 - 5, '15', 'green', 'y2');
+    createLine(svg, '10', height / 2, width, height / 2, 'red', 'x');
+    createLine(svg, width, height / 2, width - 5, height / 2 - 5, 'red', 'x1');
+    createLine(svg, width, height / 2, width - 5, height / 2 + 5, 'red', 'x2');
+}
+
+function createLine(container, x1, y1, x2, y2, color, id) {
+    let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttribute('id', id);
+    line.setAttribute('x1', x1);
+    line.setAttribute('y1', y1);
+    line.setAttribute('x2', x2);
+    line.setAttribute('y2', y2);
+    line.setAttribute("stroke", color);
+    container.appendChild(line);
+}
